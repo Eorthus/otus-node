dd09dd46b07e7a52ee2ffedf1e57956919102767
eorthus <oakenshielddis@gmail.com>
2023-12-04 20:57:39 +0400

    #feat: tree module

---
+++ index.js
@@ -0,0 +1,54 @@
+const fs = require('fs');
+const commandLineUsage = require('command-line-usage')
+const commandLineArgs = require('command-line-args')
+const treeFunction = require('./module');
+
+const optionList = [
+    {
+        name: 'path',
+        alias: 'p',
+        required: true,
+        type:String,
+        description: 'Folder path. Required.'
+    },
+    {
+        name: 'depth',
+        alias: 'd',
+        type: Number,
+        description: 'Depth of tree. E.g. 3'
+    },
+]
+
+const usageNotes = [
+    {
+        header: 'Options',
+        optionList: optionList
+    }
+]
+
+const usage = commandLineUsage(usageNotes)
+let options = null;
+try {
+    options = commandLineArgs(optionList)
+} catch(e) {
+    console.log(usage);
+    return;
+}
+
+if (Object.keys(options).length === 0 || options.help || !options.path) {
+    console.log(usage)
+    return;
+}
+
+if (!fs.existsSync(options.path)) {
+    console.log(`! Can't find directory !`);
+    console.log(usage)
+    return;
+}
+
+try {
+    treeFunction(options.path, options.depth)
+} catch(e) {
+    console.log(e);
+    console.log(usage);
+}
---
+++ module.js
@@ -0,0 +1,48 @@
+const fs = require('fs')
+const path = require('path')
+
+const setLevelLinesHandler = (counter) => {
+    let line = '_'
+    let spacer = ''
+
+    for (let i = 1; i < counter; i++) {
+        line += '_'
+    }
+    for (let i = 1; i < counter; i++) {
+        spacer += '  '
+    }
+
+    return spacer+'|'+line
+}
+
+const printPathHandler = (parentFolder, childFolder, depth, counter) => {
+    if (depth === 0) {
+        return
+    }
+    const childPath = path.join(parentFolder, childFolder)
+    const levelRouteLines = setLevelLinesHandler(counter)
+    console.log(levelRouteLines, childFolder)
+    if (fs.lstatSync(childPath).isDirectory()) {
+        {
+            const newParent = fs.readdirSync(childPath)
+
+            newParent?.map(el => {
+                printPathHandler(childPath, el, depth - 1, counter + 1)
+            })
+        }
+    }
+}
+
+const treeFunction = (folderPath, depth)=>{
+    const defaultDepth = depth?? 2
+    const folderArray = fs.readdirSync(folderPath)
+    const counter = 1
+    
+    console.log(path.basename(folderPath))
+    
+    folderArray?.map(el => {
+        printPathHandler(folderPath, el, defaultDepth, counter)
+    })
+}
+
+module.exports = treeFunction;
